288220f0f819002b3b89e0fc16309fb1bbbc11c1
Elena Kashtanova <lenachestnut@gmail.com>
2020-07-19 19:53:04 +0300

    Update lodash

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import eventHandler from './eventHandlers.js'
-import ProjectFactory from './projectController.js'
-import TaskFactory from './taskController.js'
+import eventHandler from "./eventHandlers.js";
+import ProjectFactory from "./projectController.js";
+import TaskFactory from "./taskController.js";
 
-PubSub.publish('Change active project', {
+PubSub.publish("Change active project", {
     projectIndex: 0,
 });
